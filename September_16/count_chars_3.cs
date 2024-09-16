51f7deb342c96864009514939ced5279a94867a8
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-09-16 09:40:40 +0300

    new string type

---
+++ September_16/count_chars_3.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class MainClass
+{
+    public static void Main(string[] args)    {
+        Console.WriteLine ("Pirmā teksta līnija");
+        Console.WriteLine ("Otrā teksta līnija");
+        Console.WriteLine (Add(5, 6));
+        Console.WriteLine ($"Teksts ir {CharacterCount("Labdien!")}");
+    }
+    public static int Add(int a, int b){
+        return a + b;
+    }
+    public static string CharacterCount(string text) {
+        if (text.Length > 9) {
+            return "long";
+        } else if (text.Length > 4) {
+            return "medium";
+        } else {
+            return "short";
+        }
+    }
+}
