9d893bfa2faa08d729ef58332133908cbb57a316
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-09-17 09:37:26 +0300

    made instances private, created constructor

--- September_17/bank_app.cs
+++ September_17/bank_app.cs
@@ -5,11 +5,7 @@ public class Bank
     public static void Main(string[] args)
     {
         Console.WriteLine ("Welcome to your bank!");
-        var client1 = new Client();
-        client1.id = 12345;
-        client1.name = "Jānis";
-        client1.surname = "Bērziņš";
-        client1.accountNumber = "LV198198198";
+        var client1 = new Client(12345,"Jānis","Bērziņš","LV198198198");
         client1.RequestInfo();
         Client.Greetings();
     }
@@ -17,13 +13,21 @@ public class Bank
 public class Client {
     public static int clientCount = 0;
 
-    public int id;
-    public string name;
-    public string surname;
-    public string accountNumber;
+    private int _id;
+    private string _name;
+    private string _surname;
+    private string _accountNumber;
     
+    // konstruktors
+    public Client(int id, string name, string surname, string accountNumber) {
+        _id = id;
+        _name = name;
+        _surname = surname;
+        _accountNumber = accountNumber;
+    }
+
     public void RequestInfo() {
-        Console.WriteLine($"{id} {name} {surname} {accountNumber}");
+        Console.WriteLine($"{_id} {_name} {_surname} {_accountNumber}");
     }
 
     public static void Greetings() {
