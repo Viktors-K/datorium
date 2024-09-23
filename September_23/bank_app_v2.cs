54ddcdc2caaae6582267502b76e5393865d47f58
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-09-23 10:29:52 +0300

    final changes for the day

--- September_23/bank_app_v2.cs
+++ September_23/bank_app_v2.cs
@@ -9,21 +9,22 @@ public class Bank
 		
         var client1 = new Client(12345,"Janis","Berzins");
         var client2 = new Client(12346, "Biruta", "Ozola");
-        
-		var account1 = new Account("LV1234567890123", "EUR");
-		var account2 = new Account("US1234567890124", "USD");
+
+		client1.AddAccount(new Account("LV1234567890123", "EUR"));
+		client1.AddAccount(new Account("US1234567890124", "USD"));
 		
-		client1.AddAccount(account1);
-		client1.AddAccount(account2);
+		client1.PrintAccounts();
     }
 }
 public class Client {
+    // definicija
     public static int clientCount = 0;
 
     private int _id;
     private string _name;
     private string _surname;
 
+    // kontu listes definicija
     private List<Account> _accountsList = new List<Account>();
 
     // konstruktors
@@ -31,21 +32,39 @@ public class Client {
         _id = id;
         _name = name;
         _surname = surname;
+        clientCount++;
     }
 
+    // info drukasanas metode
     public void RequestInfo() {
         Console.WriteLine($"Client ID:{_id} Name:{_name} Surname:{_surname}");
     }
 	
+    // kontu pievienosanas metode
 	public void AddAccount(Account account) {
 		_accountsList.Add(account);
 	}
+    
+    // klienta kontu izvada metode 
+    public void PrintAccounts() {
+        Console.WriteLine($"Client {_name} {_surname} has the following accounts:");
+        foreach (Account account in _accountsList) {
+            Console.WriteLine($"Number: {account.AccountNumber}.");
+        }
+    }
+
+    // klientu skaita izvada metode
+    public static void RequestClientCount() {
+        Console.WriteLine($"There are a total of {clientCount} accounts.");
+    }
 }
 
 public class Account {
+    // definicija
     private string _accountNumber;
     private string _accountCurrency;
 
+    // accountNumber parbaude
     public string AccountNumber {
         get {
             return _accountNumber;
@@ -65,6 +84,7 @@ public class Account {
         }
     }
 
+    // konstruktors
     public Account(string accountNumber, string accountCurrency){
         AccountNumber = accountNumber;
         _accountCurrency = accountCurrency;
