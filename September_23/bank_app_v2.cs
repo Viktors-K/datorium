d784f29f6c012374590c834ba430f9abc942dcaf
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-09-23 08:39:18 +0300

    Created structure

---
+++ September_23/bank_app_v2.cs
@@ -0,0 +1,53 @@
+using System;
+
+public class Bank
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Welcome to your bank!");
+        var client1 = new Client(12345,"Janis","Berzins","LV198198198");
+        client1.RequestInfo();
+
+        client1.AccountNumber = "LT354345345";
+        Console.WriteLine(client1.AccountNumber);
+        Client.Greetings();
+    }
+}
+public class Client {
+    public static int clientCount = 0;
+
+    private int _id;
+    private string _name;
+    private string _surname;
+    private string _accountNumber;
+    // paliks par property, nav metode
+    public string AccountNumber {
+        get {
+            return _accountNumber;
+        }
+
+        set {
+            if (string.IsNullOrEmpty(value)) {
+                Console.WriteLine("Error, you cannot assign empty value!");
+            } else {
+                _accountNumber = value;
+            }
+        }
+    }
+
+    // konstruktors
+    public Client(int id, string name, string surname, string accountNumber) {
+        _id = id;
+        _name = name;
+        _surname = surname;
+        _accountNumber = accountNumber;
+    }
+
+    public void RequestInfo() {
+        Console.WriteLine($"{_id} {_name} {_surname} {_accountNumber}");
+    }
+
+    public static void Greetings() {
+        Console.WriteLine("Hello, I am a Client Class!");
+    }
+}
