cb14ab42ca6819a752301b8e9be5f841e4027db8
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-10-14 09:44:28 +0300

    asd

--- October_14/uzd_2.cs
+++ October_14/uzd_2.cs
@@ -4,7 +4,17 @@ public class App
 {
 	public static void Main()
 	{
-		Console.WriteLine("Hello World");
+		var kubg = new Kubs(10, "zala");
+		var kubr = new Kubs(1, "sarkana");
+		Console.WriteLine(kubg.Krasa);
+		Console.WriteLine(kubg.AprekinatTilpumu());
+		Console.WriteLine(kubr.MalasGarums);
+		
+		kubr = null;
+		if (kubr == null) {
+			Console.WriteLine("kubr dead");
+		}
+		Console.WriteLine(kubg.MalasGarums);
 	}
 }
 
@@ -13,9 +23,12 @@ public class Kubs
     public int MalasGarums;
     public string Krasa;
     
-    public Kubs(int malasGarums, string krasa)
-    {
-        MalasGarums = malasGarums;
+    public Kubs(int malasGarums, string krasa) {
+        if (malasGarums >= 2 && malasGarums <= 10) {
+            MalasGarums = malasGarums;
+        } else {
+            MalasGarums = 2;
+        }
         Krasa = krasa;
     }
 
@@ -25,5 +38,16 @@ public class Kubs
     
     public int AprekinatTilpumu() {
 		return MalasGarums * MalasGarums * MalasGarums;
+	}
+}
+public class Bloks : Kubs {
+	private int _kubuSkaits;
+	public string Nosaukums {
+		get {
+			return Krasa + _kubuSkaits.ToString();
+		}
+	}
+	public Bloks(int malasGarums, string krasa) : base(malasGarums, krasa) {
+	
 	}
 }
