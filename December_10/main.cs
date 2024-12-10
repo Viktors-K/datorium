7e7c3a2df7faa325180980b06b3f858c202f7fbb
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-12-10 09:23:52 +0200

    some updates

--- December_10/main.cs
+++ December_10/main.cs
@@ -1,64 +1,29 @@
 using Microsoft.Data.Sqlite;
-using Microsoft.Data.SqlClient;
 using System;
 
 class Program {
     static void Main(string[] args) {
-        // Connection string for SQLite
         string connectionString = "Data Source=pizza_store.db";
         try
         {
-            // Create and open the SQLite connection
-            using (var connection = new SqliteConnection(connectionString))
-            {
-                connection.Open();
-
-                // Create table if it doesn't exist
-                var createTableCmd = connection.CreateCommand();
-                createTableCmd.CommandText = @"
-                    CREATE TABLE IF NOT EXISTS Pizzas (
-                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        Name TEXT NOT NULL,
-                        Size TEXT NOT NULL,
-                        Price REAL NOT NULL
-                    );
-                ";
-                createTableCmd.ExecuteNonQuery();
-                Console.WriteLine("Table created or already exists.");
-                
-                //Promt user for pizza name
-                Console.WriteLine("Please enter Pizza name");
-                string pizzaName = Console.ReadLine();
-                
-                //Promt user for pizza size
-                Console.WriteLine("Please enter Pizza size");
-                string pizzaSize = Console.ReadLine();
-                
-                //Promt user for pizza price
-                Console.WriteLine("Please enter Pizza price");
-                double pizzaPrice = Convert.ToDouble(Console.ReadLine());
-				
-				// Insert data in SQL lite database
-                var insertCmd = connection.CreateCommand();
-                insertCmd.CommandText = "INSERT INTO Pizzas(Name, Size, Price) VALUES (@name, @size, @price)";
-                insertCmd.Parameters.AddWithValue("@name", pizzaName);
-                insertCmd.Parameters.AddWithValue("@size", pizzaSize);
-                insertCmd.Parameters.AddWithValue("@price", pizzaPrice);
-                
-                insertCmd.ExecuteNonQuery();
-                Console.WriteLine("PIZZA IS ADDED TO DATABASE");
-                
-                // Query to display pizza database elements
-                var selectCmd = connection.CreateCommand();
-                selectCmd.CommandText = "SELECT * FROM PIZZAS";
-                
-                using (var reader = selectCmd.ExecuteReader()) {
-                    Console.WriteLine("Pizza List:");
-                    while(reader.Read()) {
-                        Console.WriteLine($"Id: {reader["Id"]}, Name: {reader["Name"]}, Size: {reader["Size"]}");
-                    }
-                }
-            }
+            var controller1 = new PizzaCtrl(connectionString);
+			while (true) {
+				Console.WriteLine("Choose option: 'add', 'print' or 'stop'.");
+				var userCommand = Console.ReadLine();
+				switch (userCommand) {
+					case "add":
+						controller1.AddPizza();
+						break;
+					case "print":
+						controller1.PrintPizzas();
+						break;
+					case "stop":
+						return;
+					default:
+						Console.WriteLine("Invalid option.");
+						break;
+				}
+			}
         }
         catch (Exception ex) {
             Console.WriteLine("Test");
@@ -107,6 +72,23 @@ class Program {
                 selectCmd.CommandText = "SELECT * FROM PIZZAS";
 				return selectCmd.ExecuteReader();
 			}
+		}
+		private void AddPizza() {
+			Console.WriteLine("Please enter Pizza name");
+			string pizzaName = Console.ReadLine();
+			Console.WriteLine("Please enter Pizza size");
+			string pizzaSize = Console.ReadLine();
+			Console.WriteLine("Please enter Pizza price");
+			double pizzaPrice = Convert.ToDouble(Console.ReadLine());
+			this.AddPizzaToTable(pizzaName,pizzaSize,pizzaPrice);
+		}
+		private void PrintPizzas() {
+			using (var reader = this.GetAllPizzasFromTable()) {
+				Console.WriteLine("Pizza List:");
+				while(reader.Read()) {
+					Console.WriteLine($"Id: {reader["Id"]}, Name: {reader["Name"]}, Size: {reader["Size"]}");
+				}
+			}	
 		}
 	}
 }
