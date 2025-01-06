dbb15914ca6e14a16d7af94fbda3861f9da5f77e
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2025-01-06 09:59:51 +0200

    rgb color picker

--- c_sharp_projects/FirstApp/FirstApp/Form1.Designer.cs
+++ c_sharp_projects/FirstApp/FirstApp/Form1.Designer.cs
@@ -28,46 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            numericUpDown1 = new NumericUpDown();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            numericUpDownRed = new NumericUpDown();
+            pictureBoxRed = new PictureBox();
+            numericUpDownGreen = new NumericUpDown();
+            numericUpDownBlue = new NumericUpDown();
+            pictureBoxGreen = new PictureBox();
+            pictureBoxBlue = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownRed).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxRed).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownGreen).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownBlue).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxGreen).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxBlue).BeginInit();
             SuspendLayout();
             // 
-            // numericUpDown1
+            // numericUpDownRed
             // 
-            numericUpDown1.Location = new Point(29, 39);
-            numericUpDown1.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(150, 27);
-            numericUpDown1.TabIndex = 0;
-            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            numericUpDownRed.Location = new Point(29, 39);
+            numericUpDownRed.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            numericUpDownRed.Name = "numericUpDownRed";
+            numericUpDownRed.Size = new Size(150, 27);
+            numericUpDownRed.TabIndex = 0;
+            numericUpDownRed.ValueChanged += numericUpDownRed_ValueChanged;
             // 
-            // pictureBox1
+            // pictureBoxRed
             // 
-            pictureBox1.Location = new Point(29, 124);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(150, 141);
-            pictureBox1.TabIndex = 1;
-            pictureBox1.TabStop = false;
+            pictureBoxRed.Location = new Point(29, 124);
+            pictureBoxRed.Name = "pictureBoxRed";
+            pictureBoxRed.Size = new Size(150, 141);
+            pictureBoxRed.TabIndex = 1;
+            pictureBoxRed.TabStop = false;
+            // 
+            // numericUpDownGreen
+            // 
+            numericUpDownGreen.Location = new Point(281, 39);
+            numericUpDownGreen.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            numericUpDownGreen.Name = "numericUpDownGreen";
+            numericUpDownGreen.Size = new Size(150, 27);
+            numericUpDownGreen.TabIndex = 0;
+            numericUpDownGreen.ValueChanged += numericUpDownGreen_ValueChanged;
+            // 
+            // numericUpDownBlue
+            // 
+            numericUpDownBlue.Location = new Point(549, 39);
+            numericUpDownBlue.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            numericUpDownBlue.Name = "numericUpDownBlue";
+            numericUpDownBlue.Size = new Size(150, 27);
+            numericUpDownBlue.TabIndex = 0;
+            numericUpDownBlue.ValueChanged += numericUpDownBlue_ValueChanged;
+            // 
+            // pictureBoxGreen
+            // 
+            pictureBoxGreen.Location = new Point(281, 124);
+            pictureBoxGreen.Name = "pictureBoxGreen";
+            pictureBoxGreen.Size = new Size(150, 141);
+            pictureBoxGreen.TabIndex = 1;
+            pictureBoxGreen.TabStop = false;
+            // 
+            // pictureBoxBlue
+            // 
+            pictureBoxBlue.Location = new Point(549, 124);
+            pictureBoxBlue.Name = "pictureBoxBlue";
+            pictureBoxBlue.Size = new Size(150, 141);
+            pictureBoxBlue.TabIndex = 1;
+            pictureBoxBlue.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(pictureBox1);
-            Controls.Add(numericUpDown1);
+            Controls.Add(pictureBoxBlue);
+            Controls.Add(pictureBoxGreen);
+            Controls.Add(pictureBoxRed);
+            Controls.Add(numericUpDownBlue);
+            Controls.Add(numericUpDownGreen);
+            Controls.Add(numericUpDownRed);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownRed).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxRed).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownGreen).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownBlue).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxGreen).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxBlue).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private NumericUpDown numericUpDown1;
-        private PictureBox pictureBox1;
+        private NumericUpDown numericUpDownRed;
+        private PictureBox pictureBoxRed;
+        private NumericUpDown numericUpDownGreen;
+        private NumericUpDown numericUpDownBlue;
+        private PictureBox pictureBoxGreen;
+        private PictureBox pictureBoxBlue;
     }
 }
--- c_sharp_projects/FirstApp/FirstApp/Form1.cs
+++ c_sharp_projects/FirstApp/FirstApp/Form1.cs
@@ -7,10 +7,20 @@ namespace FirstApp
             InitializeComponent();
         }
 
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        private void numericUpDownRed_ValueChanged(object sender, EventArgs e)
         {
-            int redValue = (int)numericUpDown1.Value;
-            pictureBox1.BackColor = Color.FromArgb(redValue, 0, 0);
+            int redValue = (int)numericUpDownRed.Value;
+            pictureBoxRed.BackColor = Color.FromArgb(redValue, 0, 0);
+        }
+        private void numericUpDownBlue_ValueChanged(object sender, EventArgs e)
+        {
+            int blueValue = (int)numericUpDownBlue.Value;
+            pictureBoxBlue.BackColor = Color.FromArgb(0, 0, blueValue);
+        }
+        private void numericUpDownGreen_ValueChanged(object sender, EventArgs e)
+        {
+            int greenValue = (int)numericUpDownGreen.Value;
+            pictureBoxGreen.BackColor = Color.FromArgb(0, greenValue, 0);
         }
     }
 }
