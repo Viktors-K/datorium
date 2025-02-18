fba9b0000feff3f06c11ebe083173ca6770cf730
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2025-02-18 09:14:32 +0200

    new asp.net api project

---
+++ c_sharp_projects/datoriummerch-master/datoriummerch/Controllers/MerchController.cs
@@ -0,0 +1,27 @@
+﻿using datoriummerch.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datoriummerch.Controllers
+{
+    [Route("api/datorium-merch")]
+    [ApiController]
+    public class MerchController
+    {
+        private readonly MerchContext _dbContext;
+        public MerchController(MerchContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet("ok")] // api/datorium-merch/ok
+        public ActionResult<string> GetOk()
+        {
+            return "OK";
+        }
+        [HttpGet] // api/datorium-merch
+        public ActionResult<IEnumerable<Merch>> GetMerch()
+        {
+            return _dbContext.Merches.ToList();
+        }
+    }
+}
