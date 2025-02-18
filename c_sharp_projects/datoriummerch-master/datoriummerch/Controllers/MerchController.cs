76b70b5712576d430c374031cab3ccacd7ae4f09
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2025-02-18 09:29:33 +0200

    Created POST method for merchapi

--- c_sharp_projects/datoriummerch-master/datoriummerch/Controllers/MerchController.cs
+++ c_sharp_projects/datoriummerch-master/datoriummerch/Controllers/MerchController.cs
@@ -5,7 +5,7 @@ namespace datoriummerch.Controllers
 {
     [Route("api/datorium-merch")]
     [ApiController]
-    public class MerchController
+    public class MerchController: ControllerBase
     {
         private readonly MerchContext _dbContext;
         public MerchController(MerchContext context)
@@ -22,6 +22,14 @@ namespace datoriummerch.Controllers
         public ActionResult<IEnumerable<Merch>> GetMerch()
         {
             return _dbContext.Merches.ToList();
+        }
+        [HttpPost]
+        public ActionResult<Merch> PostMerch(Merch merch)
+        {
+            _dbContext.Merches.Add(merch);
+            _dbContext.SaveChanges();
+
+            return Created();
         }
     }
 }
