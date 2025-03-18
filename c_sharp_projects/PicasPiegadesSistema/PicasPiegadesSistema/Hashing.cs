aed760fcbc8e2749a22f922330cf83ff6314c531
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2025-03-18 09:30:03 +0200

    all the work for today

---
+++ c_sharp_projects/PicasPiegadesSistema/PicasPiegadesSistema/Hashing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicasPiegadesSistema
+{
+    class Hashing
+    {
+        
+        public static string GeneratePasswordHash(string password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+            byte[] hashedBytes = HashAlgorithm.Create("SHA1").ComputeHash(bytes);
+
+            return Convert.ToBase64String(hashedBytes);
+        }
+        public static bool CheckPasswordHash(string password, string hash) 
+        {
+            string passowrdHash = GeneratePasswordHash(password);
+            return passowrdHash.Equals(hash);
+        }
+    }
+}
---
+++ c_sharp_projects/PicasPiegadesSistema/PicasPiegadesSistema/UserDb.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace PicasPiegadesSistema
+{
+    class UserDb
+    {
+        private readonly string _connectionString;
+        public UserDb(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+        public void CreateUserTable()
+        {
+            using (var connection = new SqliteConnection(this._connectionString))
+            {
+                connection.Open();
+                var createTableCmd = connection.CreateCommand();
+                createTableCmd.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS Users (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Username TEXT NOT NULL UNIQUE,
+                        PasswordHash TEXT NOT NULL
+                    )
+                   ";
+                createTableCmd.ExecuteNonQuery();
+            }
+        }
+        public void CreateUser(string username, string password)
+        {
+            using (var connection = new SqliteConnection(this._connectionString))
+            {
+                connection.Open();
+                var createUserCmd = connection.CreateCommand();
+                createUserCmd.CommandText = @"
+                    INSERT INTO Users(Username, Password)
+                    VALUES (@username, @password)
+                    )
+                   ";
+                //createUserCmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
